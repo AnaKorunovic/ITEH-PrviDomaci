c79c57d1d91d1ff2b34e7bda1554e2e9561469e3
AnaKorunovic <korunovicana9@gmail.com>
2022-02-01 20:00:49 +0100

    dodata je CRUD operacija izmene kafe

---
+++ server/kafa/update.php
@@ -0,0 +1,25 @@
+<?php
+    require '../broker.php';
+    $broker=Broker::getBroker();
+
+    $id=$_POST['id'];
+    $naziv=$_POST['naziv'];
+    $tezina=$_POST['tezina'];
+    $ukus=$_POST['ukus'];
+    $marka_id=$_POST['marka_id'];
+    $opis=$_POST['opis'];
+   
+    
+    $upit="Update kafa set naziv='".$naziv."', tezina=".$tezina.", marka_id=".$marka_id.", opis='".$opis."',
+    ukus='".$ukus."' where id=".$id;
+    
+    if(!isset($id)){
+        header('Location: ../../update.php&id='.$id.'&greska=Nije prosledjen id');
+        exit;
+    }
+   
+    $broker->izvrsiUpit( $upit);
+    header('Location: ../../index.php');
+
+
+?>
