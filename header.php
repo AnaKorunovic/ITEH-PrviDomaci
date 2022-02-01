5d7e347c4c23827ccdf4a742c53e8f43969e6c81
AnaKorunovic <korunovicana9@gmail.com>
2022-02-01 22:41:03 +0100

    dodat je header

---
+++ header.php
@@ -0,0 +1,33 @@
+<nav className="navbar">
+        <div className="navbar-container">
+        <div className='navbar-logo' >
+          <Link to="/"><img src='img/logo.png' alt='kafa' /> </Link>
+        </div>   
+         
+          
+          <ul className=nav-menu>
+            <li className="nav-item">
+              <a href="index.php" className="nav-link">
+                Početna
+</a>
+            </li>
+            <li className="nav-item">
+              <a href="dodajKafu.php" className="nav-link">
+                Dodaj kafu
+</a>
+            </li>
+            <li className="nav-item">
+              <a href="marke.php" className="nav-link">
+                Dodaj marku
+</a>
+            </li>
+            
+            
+            
+          </ul>
+          
+         
+          
+        </div>
+      </nav>
+  
